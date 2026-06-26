ca9a6f041dfd319c0cb68fa68044683f04b23040
Amanda Aulia Nurani <amanda20123043@digitechuniversity.ac.id>
2026-06-26 16:48:13 +0700

    feat: update UI home page dengan tema warna dinamis berdasarkan app flavor

--- lib/features/home/presentation/pages/home_page.dart
+++ lib/features/home/presentation/pages/home_page.dart
@@ -6,22 +6,36 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Menangkap apakah aplikasi berjalan di mode PROD
+    final isProd = EnvConfig.isProduction;
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('UAS App [${EnvConfig.environment}]'),
-        // Jika Production warnanya Hijau, jika Dev warnanya BlueGrey (Abu-abu biru)
-        backgroundColor: EnvConfig.isProduction ? Colors.green : Colors.blueGrey,
+        // Menampilkan judul dinamis sesuai environment dan data personal kamu
+        title: Text(isProd ? 'UTD - 20123043' : 'DEV - Amanda'),
+        // Jika PROD wajib Biru Gelap (Indigo/Blue Dark), jika DEV abu-abu-biru bebas
+        backgroundColor: isProd ? const Color(0xFF0D47A1) : Colors.blueGrey,
         foregroundColor: Colors.white,
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Icon(Icons.rocket_launch, size: 100, color: Colors.blueAccent),
+            Icon(
+              Icons.newspaper, 
+              size: 100, 
+              color: isProd ? const Color(0xFF0D47A1) : Colors.blueAccent
+            ),
             const SizedBox(height: 20),
-            const Text('Skeleton Project Siap!', style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+            Text(
+              isProd ? 'Production Mode' : 'Development Mode', 
+              style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold)
+            ),
             const SizedBox(height: 10),
-            Text('Base URL: ${EnvConfig.baseUrl}', style: const TextStyle(color: Colors.grey)),
+            const Text(
+              'Tema: DigiNews Offline-First', 
+              style: TextStyle(color: Colors.grey)
+            ),
           ],
         ),
       ),
